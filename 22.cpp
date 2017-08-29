78e126fa44c113ac24f831ef170ce864675fa2d6
abhattacharjeee <31150949+abhattacharjeee@users.noreply.github.com>
2017-08-29 17:30:59 +0530

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a;
+	cout<<"Hi.Enter the number";
+	cin>>a;
+	cout<<"Remember,your journey is towards infinity.So why are you limiting you in"<<a;
+	return 0;
+}
