3271902a65f592ca2fc7a5f66023c83b4c939aef
abhattacharjeee <31150949+abhattacharjeee@users.noreply.github.com>
2017-08-29 15:36:46 +0530

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float 1st angle,2nd angle,3rd angle;
+cout<< "Enter the 1st angle of the triangle=";
+cin>>1st angle;
+cout<< "Enter the 2nd angle of the triangle=";
+cin>>2nd angle;
+3rd angle=180-(1st angle+2nd angle);
+cout<<"3rd angle of the triangle="<<3rd angle;
+	return 0;
+}
